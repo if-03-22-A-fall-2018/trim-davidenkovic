7b49510ee9e8f9c1793ce4f9056063153c293e74
Ignjatovic David <Ignjatovicdavid1@gmail.com>
2018-11-27 11:45:00 +0100

    not fisnished

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,24 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+ #include "trim.h"
+ #include <stdio.h>
+ #include <string.h>
+
+ void trim(const char* source,  char* trimed_string ){
+   char str[STRLEN];
+   char trimmed[STRLEN];
+   bool teststart;
+
+   for (int i = (int)strlen(str) ; i = 0; i--) {
+     if (str[i] == ' ') {
+       teststart = true;
+     }
+     if (teststart == true) {
+        trimmed[i] = str[i];
+     }
+     else{
+       strcpy(trimmed,"");
+     }
+   }
+}
--- trim.h
+++ trim.h
@@ -10,3 +10,12 @@
  * Interface for trim
  * ----------------------------------------------------------
  */
+
+#ifndef ___TRIM_H
+#define  ___TRIM_H
+
+#define STRLEN 16
+
+ void trim(const char* source,  char* trimed_string );
+
+#endif
